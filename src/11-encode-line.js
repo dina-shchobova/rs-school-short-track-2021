8bfa588ebbc96e7b886ec2f84626dd172738dcbb
Dina Shchobova <dina.shchobova@gmail.com>
2021-04-11 19:03:20 +0300

    init: solve 11 and 15

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const rezult = [];
+  for (let i = 0; i < arr.length; i++) {
+    let counter = 1;
+    while (arr[i] === arr[i + 1]) {
+      arr.splice(i + 1, 1);
+      counter++;
+    }
+    if (counter > 1) {
+      rezult.push(`${counter}${arr[i]}`);
+    } else {
+      rezult.push(`${arr[i]}`);
+    }
+  }
+  return rezult.join('');
 }
 
 module.exports = encodeLine;
