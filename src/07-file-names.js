704ec2b650082158bf2e26c05a87e8772f522c38
Dina Shchobova <dina.shchobova@gmail.com>
2021-04-11 03:51:21 +0300

    init: solve 7

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,18 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const rezult = names.map((k) => k);
+  for (let i = 0; i < rezult.length; i++) {
+    let counter = 0;
+    for (let j = 1; j < rezult.length; j++) {
+      if ((i !== j) && rezult[i] === rezult[j]) {
+        counter++;
+        rezult[j] += `(${counter})`;
+      }
+    }
+  }
+  return rezult;
 }
 
 module.exports = renameFiles;
