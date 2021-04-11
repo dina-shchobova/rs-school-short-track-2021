4b6c05f1f5b40fdc6704531fff72d0c14eaae5e9
Dina Shchobova <dina.shchobova@gmail.com>
2021-04-11 21:15:05 +0300

    init: solve 12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,34 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const appearances = {};
+  const arr = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (let item of domains) {
+    arr.push(`${item}.`);
+    while (item.indexOf('.') !== (-1)) {
+      const index = item.indexOf('.') + 1;
+      item = item.slice(index);
+      arr.push(`${item}.`);
+    }
+  }
+  arr.sort();
+  const reverseArr = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const item of arr) {
+    const reverse = item.split('.').reverse().join('.');
+    reverseArr.push(reverse);
+  }
+
+  for (let i = 0; i < reverseArr.length; i++) {
+    if (appearances[reverseArr[i]] === undefined) {
+      appearances[reverseArr[i]] = 1;
+    } else {
+      appearances[reverseArr[i]]++;
+    }
+  }
+  return appearances;
 }
 
 module.exports = getDNSStats;
